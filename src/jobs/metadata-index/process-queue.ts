26c5a400e1cf97bd044418fcd7a25fcd4952ba4c
Leif <leifleiser@MacBook-Pro-von-Leif.local>
2022-08-29 16:23:40 +0200

    chore: tailoring metadata fetching for polymon

--- src/jobs/metadata-index/process-queue.ts
+++ src/jobs/metadata-index/process-queue.ts
@@ -1,14 +1,14 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 import axios from "axios";
+import promiseRetry from "promise-retry";
 import _ from "lodash";
 import { Job, Queue, QueueScheduler, Worker } from "bullmq";
 import { randomUUID } from "crypto";
 
 import { logger } from "@/common/logger";
-import { redis, extendLock, releaseLock, acquireLock, getLockExpiration } from "@/common/redis";
+import { redis, extendLock, releaseLock, getLockExpiration } from "@/common/redis";
 import { config } from "@/config/index";
-import { getNetworkName } from "@/config/network";
 import { PendingRefreshTokens } from "@/models/pending-refresh-tokens";
 import * as metadataIndexWrite from "@/jobs/metadata-index/write-queue";
 
@@ -34,10 +34,11 @@ if (config.doBackgroundWork) {
   type TokenMetadata = {
     contract: string;
     tokenId: string;
-    name?: string;
-    description?: string;
-    imageUrl?: string;
+    name: string;
+    description: string;
+    imageUrl: string;
     mediaUrl?: string;
+    collection: string;
     attributes: {
       key: string;
       value: string;
@@ -51,14 +52,10 @@ if (config.doBackgroundWork) {
     async (job: Job) => {
       const { method } = job.data;
 
-      let useMetadataApiBaseUrlAlt = false;
-
       const rateLimitExpiresIn = await getLockExpiration(getRateLimitLockName(method));
 
       if (rateLimitExpiresIn > 0) {
         logger.info(QUEUE_NAME, `Rate Limited. rateLimitExpiresIn: ${rateLimitExpiresIn}`);
-
-        useMetadataApiBaseUrlAlt = true;
       }
 
       const count = 20;
@@ -69,7 +66,6 @@ if (config.doBackgroundWork) {
       // Get the tokens from the list
       const pendingRefreshTokens = new PendingRefreshTokens(method);
       const refreshTokens = await pendingRefreshTokens.get(count);
-      const tokenToCollections = {};
 
       // If no more tokens
       if (_.isEmpty(refreshTokens)) {
@@ -77,53 +73,51 @@ if (config.doBackgroundWork) {
       }
 
       // Build the query string and store the collection for each token
+      // Build the query string and store the collection for each token
+      const metadatas: TokenMetadata[] = [];
+
       for (const refreshToken of refreshTokens) {
-        queryParams.append("token", `${refreshToken.contract}:${refreshToken.tokenId}`);
-        (tokenToCollections as any)[`${refreshToken.contract}:${refreshToken.tokenId}`] =
-          refreshToken.collection;
-      }
+        // Get the metadata for the tokens
+        const url = `https://meta.polkamon.com/meta?id=${refreshToken.tokenId}`;
 
-      // Get the metadata for the tokens
-      const url = `${
-        useMetadataApiBaseUrlAlt ? config.metadataApiBaseUrlAlt : config.metadataApiBaseUrl
-      }/v4/${getNetworkName()}/metadata/token?${queryParams.toString()}`;
-
-      let metadataResult;
-
-      try {
-        metadataResult = await axios.get(url, { timeout: 60 * 1000 }).then(({ data }) => data);
-      } catch (error) {
-        if ((error as any).response?.status === 429) {
-          logger.info(
-            QUEUE_NAME,
-            `Too Many Requests. useMetadataApiBaseUrlAlt=${useMetadataApiBaseUrlAlt}, error: ${JSON.stringify(
-              (error as any).response.data
-            )}`
-          );
-
-          await pendingRefreshTokens.add(refreshTokens, true);
-
-          if (!useMetadataApiBaseUrlAlt) {
-            await acquireLock(getRateLimitLockName(method), 5);
+        await promiseRetry(async (retry) => {
+          try {
+            const metadata: any = await axios
+              .get(url, { timeout: 60 * 1000 })
+              .then(({ data }) => data);
+            metadatas.push({
+              name: metadata.name,
+              description: metadata.description,
+              imageUrl: metadata.image,
+              mediaUrl: metadata.animation_url,
+              attributes: metadata?.attributes.map((el: any) => {
+                let kind: string;
+                if (el.display_type === "number") {
+                  kind = "number";
+                } else if (el.display_type === "date") {
+                  kind = "number";
+                } else {
+                  kind = "string";
+                }
+                return {
+                  key: el.trait_type,
+                  value: String(el.value),
+                  kind,
+                };
+              }),
+              ...refreshToken,
+            });
+          } catch (e) {
+            // eslint-disable-next-line no-console
+            console.log(e);
+            return retry(e);
           }
-
-          if (await extendLock(getLockName(method), 60 * 5)) {
-            await addToQueue(method);
-          }
-
-          return;
-        }
-
-        throw error;
+        });
       }
-
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const metadata: TokenMetadata[] = (metadataResult as any).metadata;
 
       await metadataIndexWrite.addToQueue(
-        metadata.map((m) => ({
+        metadatas.map((m: TokenMetadata) => ({
           ...m,
-          collection: (tokenToCollections as any)[`${m.contract.toLowerCase()}:${m.tokenId}`],
         }))
       );
 
