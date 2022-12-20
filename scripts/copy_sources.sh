05c41f8b266c17e56ec5e5a57b95c9a6807ca9fe
Mike Lohmann <mike@rocknitive.com>
2022-12-20 10:31:09 +0100

    Add a delete-before option to rsync to avoid old files existing on target

--- scripts/copy_sources.sh
+++ scripts/copy_sources.sh
@@ -8,6 +8,7 @@ IPV4_LIST=$1;
 for IPV4 in ${IPV4_LIST}; do
   scp ./env-file c3labs@$IPV4:${REMOTE_HOME}/env-indexer;
   rm ./env-file
-  rsync -a ./ c3labs@$IPV4:${REMOTE_REPO_BASE_DIR};
+  # Delete files on TARGET before copying
+  rsync -a --delete-before ./ c3labs@$IPV4:${REMOTE_REPO_BASE_DIR};
   sleep 1;
 done;
